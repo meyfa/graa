d1372f75f9897518dfdf8528b64e1954cbfb0cc4
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-04-21 21:35:47 +0200

    feat: Add repo iteration and simple job for updating LICENSE year
    This adds code to index.ts that iterates over all of the user's repos,
    and for each, executes a set of automations. One such automation is
    implemented (although only very crudely). Its job is to look at the
    last commit date, and see if it is newer than the year range specified
    in the LICENSE file, and if so, create a pull request to update the
    year range in LICENSE. There is absolutely no error handling or
    checking for already existing branches or PRs yet. Hence, the CI step
    to execute the script is disabled for safety.

---
+++ lib/types.ts
@@ -0,0 +1,13 @@
+import { GetResponseDataTypeFromEndpointMethod } from '@octokit/types'
+import { Octokit } from '@octokit/core'
+import { Constructor } from '@octokit/core/dist-types/types.js'
+import { Api } from '@octokit/plugin-rest-endpoint-methods/dist-types/types.js'
+import { PaginateInterface } from '@octokit/plugin-paginate-rest'
+
+export type GraaOctokit = InstanceType<typeof Octokit & Constructor<Api> & Constructor<{ paginate: PaginateInterface }>>
+
+// this is the type of the octokit.rest object
+type RestMethods = GraaOctokit['rest']
+
+export type RepoOfAuthenticatedUser = GetResponseDataTypeFromEndpointMethod<RestMethods['repos']['listForAuthenticatedUser']>[number]
+export type BranchOfRepo = GetResponseDataTypeFromEndpointMethod<RestMethods['repos']['getBranch']>
