3b7df8e8170576544284f60e2a32cabd4a3f05ae
Fabian Meyer <3982806+meyfa@users.noreply.github.com>
2022-04-25 13:23:29 +0200

    feat: Add automation 'reconfigure' for changing repo settings (#15)
    * feat: Add automation 'reconfigure' for changing repo settings
    
    The automation can currently configure the 'delete-branch-on-merge'
    setting. This option is set to true in 'config:base', since I assume
    it is always wanted and will also improve the GRAA workflow by avoiding
    branch name conflicts.
    
    * fix(reconfigure): Add missing await

---
+++ automations/reconfigure.ts
@@ -0,0 +1,28 @@
+import { Log } from '../lib/log.js'
+import { GraaOctokit, RepoOfAuthenticatedUser } from '../lib/types.js'
+import { boolean, defaulted, object } from 'superstruct'
+import { assertAutomationOptions } from '../lib/validation.js'
+
+const Options = object({
+  'delete-branch-on-merge': defaulted(boolean(), false)
+})
+
+/**
+ * Run a job for reconfiguring a subset of the repository settings.
+ *
+ * @param log The scoped logger.
+ * @param octokit The API instance.
+ * @param repo The repo to run this action on.
+ * @param options The automation options.
+ */
+export async function reconfigure (log: Log, octokit: GraaOctokit, repo: RepoOfAuthenticatedUser, options: object): Promise<void> {
+  const opt = assertAutomationOptions(repo, Options, options)
+
+  await octokit.rest.repos.update({
+    owner: repo.owner.login,
+    repo: repo.name,
+
+    // sample setting
+    delete_branch_on_merge: opt['delete-branch-on-merge']
+  })
+}
--- configs/base.ts
+++ configs/base.ts
@@ -2,6 +2,9 @@ import { PartialConfig } from '../lib/config.js'
 
 export const baseConfig: PartialConfig = {
   automations: {
+    reconfigure: {
+      'delete-branch-on-merge': true
+    },
     'license-date': {}
   }
 }
